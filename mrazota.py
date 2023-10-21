9a182a5b0b917129550c0e53e26c03df275aa62d
imhutao <92280773+imhutao@users.noreply.github.com>
2023-10-21 16:40:41 +1000

    Update mrazota.py

--- mrazota.py
+++ mrazota.py
@@ -16,19 +16,61 @@ while True:
     ce = random.randint(0, 6)
     if ce == rock:
         print("вы столкнулись с ТЯЖЁЛЫМ РОКОМ")
-        hp -= 5
-        print("осталось хп", hp)
-        sleep(1)
+        stor = random.randint(0, 3)
+        print("попробуй увернуться! вправо - 1, влево - 2, прыжок - 3")
+        stro = int(input())
+        if stro == stor:
+            print("у тебя получилось! едем дальше")
+            sleep(1)
+        elif stro>3 or 0>=stro:
+            print("так нельзя! ты врезался")
+            hp -= 5
+            print("осталось хп", hp)
+            sleep(1)
+        elif stro != stor:
+            print("не вышло! лох")
+            hp -= 5
+            print("осталось хп", hp)
+            sleep(1)
+    
     elif ce == yetti:
         print("вы столкнулись с МОХНАТАЯ ШНЯГА")
-        hp -= 5
-        print("осталось хп", hp)
-        sleep(1)
+        stor = random.randint(0, 3)
+        print("попробуй увернуться! вправо - 1, влево - 2, прыжок - 3")
+        stro = int(input())
+        if stro == stor:
+            print("у тебя получилось! едем дальше")
+            sleep(1)
+        elif stro>3 or 0>=stro:
+            print("так нельзя! ты врезался")
+            hp -= 5
+            print("осталось хп", hp)
+            sleep(1)
+        elif stro != stor:
+            print("не вышло! лох")
+            hp -= 5
+            print("осталось хп", hp)
+            sleep(1)
+        
     elif ce == tree:
         print("вы столкнулись с АААААААААА ПРИРОДА")
-        hp -= 5
-        print("осталось хп", hp)
-        sleep(1)
+        stor = random.randint(0, 3)
+        print("попробуй увернуться! вправо - 1, влево - 2, прыжок - 3")
+        stro = int(input())
+        if stro == stor:
+            print("у тебя получилось! едем дальше")
+            sleep(1)
+        elif stro>3 or 0>=stro:
+            print("так нельзя! ты врезался")
+            hp -= 5
+            print("осталось хп", hp)
+            sleep(1)
+        elif stro != stor:
+            print("не вышло! лох")
+            hp -= 5
+            print("осталось хп", hp)
+            sleep(1)
+        
         
     speed += 1
     distance -= speed
